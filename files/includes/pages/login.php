c8df27bc9e14146bc19e1141e3d5e911835e25ec
JacobJ97 <jacobjackson977@hotmail.com>
2018-03-25 03:30:02 +1000

    change textbox name to password and make it that type

--- files/includes/pages/login.php
+++ files/includes/pages/login.php
@@ -10,8 +10,8 @@
                 <input class="form-control" type="text" name="puname1" id="punameord1" required>
             </div>
             <div class="form-group">
-                <label for="pword2">Username <span class="text-danger">*</span></label>
-                <input class="form-control" type="text" name="pword2" id="pwordord2" required>
+                <label for="pword2">Password <span class="text-danger">*</span></label>
+                <input class="form-control" type="password" name="pword2" id="pwordord2" required>
             </div>
             <button class="btn btn-primary" id="submitdeets3" type="submit">Submit <i class="fas fa-paper-plane"></i></button>
         </form>
