d15125c605ffeb543926d69a1f160bbdc70092ab
JacobJ97 <jacobjackson977@hotmail.com>
2018-03-24 20:45:27 +1000

    added login page skeleton

---
+++ files/includes/pages/login.php
@@ -0,0 +1,19 @@
+<div class="container">
+    <div class="body">
+        <div class="possible-alerts">
+
+        </div>
+        <h1>Login</h1>
+        <form class="needs-validation">
+            <div class="form-group">
+                <label for="puname1">Username <span class="text-danger">*</span></label>
+                <input class="form-control" type="text" name="puname1" id="punameord1" required>
+            </div>
+            <div class="form-group">
+                <label for="pword2">Username <span class="text-danger">*</span></label>
+                <input class="form-control" type="text" name="pword2" id="pwordord2" required>
+            </div>
+            <button class="btn btn-primary" id="submitdeets3" type="submit">Submit <i class="fas fa-paper-plane"></i></button>
+        </form>
+    </div>
+</div>
