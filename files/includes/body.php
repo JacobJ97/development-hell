401ea345cfa2ebf34d984781824e1830128f3d97
JacobJ97 <jacobjackson977@hotmail.com>
2018-03-24 20:29:32 +1000

    changed page directory to be under includes rather than public_html

--- files/includes/body.php
+++ files/includes/body.php
@@ -3,10 +3,10 @@
 $server_request = $_GET['site-page'];
 
 if (empty($server_request)) {
-    include_once($_SERVER['DOCUMENT_ROOT'] . "/pages/home.php");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "../files/includes/pages/home.php");
 }
 else {
-    $all_webpages = scandir($_SERVER['DOCUMENT_ROOT'] . "/pages", 1);
+    $all_webpages = scandir($_SERVER['DOCUMENT_ROOT'] . "../files/includes/pages/", 1);
 
     foreach ($all_webpages as &$page) {
         $page = str_replace(".php", "", $page);
@@ -18,7 +18,7 @@ else {
     for ($i = 0; $i < $num_of_webpages; $i++) {
         switch($server_request) {
             case $all_webpages[$i]:
-                include_once($_SERVER['DOCUMENT_ROOT'] . "/pages/" . $all_webpages[$i] . ".php");
+                include_once($_SERVER['DOCUMENT_ROOT'] . "../files/includes/pages/" . $all_webpages[$i] . ".php");
                 break;
 
         }
