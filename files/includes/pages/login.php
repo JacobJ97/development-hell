fc017398352f98e01c34b6335945b38275463b10
JacobJ97 <jacobjackson977@hotmail.com>
2018-04-14 01:57:48 +1000

    Added alerts "Reset password" link added (doesn't lead anywhere)

--- files/includes/pages/login.php
+++ files/includes/pages/login.php
@@ -1,10 +1,19 @@
 <div class="container">
     <div class="body">
         <div class="possible-alerts">
-
+            <div style="display: none;" role="alert" id="pwsuccessful" class="alert alert-success alert-dismissible"><strong>Success!</strong> <span class="message"></span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div style="display: none;" role="alert" id="pwfailure" class="alert alert-danger alert-dismissible"><strong>Error!</strong> <span class="message"></span>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
         </div>
         <h1>Login</h1>
-        <form class="needs-validation">
+        <form class="needs-validation" id="loginform">
             <div class="form-group">
                 <label for="puname1">Username <span class="text-danger">*</span></label>
                 <input class="form-control" type="text" name="puname1" id="punameord1" required>
@@ -17,7 +26,10 @@
                 <input class="form-check-input" value="yes" type="checkbox" name="ncheck3" id="newcheckord3">
                 <label class="form-check-label" for="ncheck3">Check if you are a new user</label>
             </div>
-            <button class="btn btn-primary" id="submitdeets3" type="submit">Submit <i class="fas fa-paper-plane"></i></button>
+            <div class="form-group">
+                <a href="#">Reset password</a>
+            </div>
+            <button class="btn btn-primary" value="password" id="submitdeets3" type="submit">Submit <i class="fas fa-paper-plane"></i></button>
         </form>
     </div>
 </div>
