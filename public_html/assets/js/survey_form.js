eb5c05f4a9e7399feb5554d63ad018205bb61ef0
JacobJ97 <jacobjackson977@hotmail.com>
2018-04-14 01:56:22 +1000

    ajax submission form for login page added

--- public_html/assets/js/survey_form.js
+++ public_html/assets/js/survey_form.js
@@ -52,4 +52,28 @@ $(document).ready(function () {
             }
         });
     });
+
+    $("#loginform").submit(function (event) {
+        event.preventDefault();
+        var form = $("#loginform");
+        $.ajax({
+            type: 'POST',
+            url: '/process.php',
+            data: form.serialize() + "&" + $.param({'form-action': document.activeElement.getAttribute('value')}),
+            dataType: 'json',
+            success: function (data) {
+                if ((data.status) === 'successful') {
+                    $('#pwsuccessful .message').html(data.reason);
+                    $('#pwsuccessful').show();
+                    setTimeout(function () {
+                        window.location.replace("/");
+                    }, 5000);
+                }
+                else {
+                    $('#pwfailure .message').html(data.reason);
+                    $('#pwfailure').show();
+                }
+            }
+        })
+    })
 });
